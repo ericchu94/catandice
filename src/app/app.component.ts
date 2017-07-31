94f08505b32644ae5f2466990d9eb32a598d2928
Eric Chu <eric@ericchu.net>
2017-07-31 13:42:02 -0400

    Implement dice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  night: boolean = false;
+  die1: number = 1;
+  die2: number = 6;
+
+  ngOnInit(): void {
+    this.onRandom();
+  }
+
+  onRandom(): void {
+    this.die1 = Math.floor(Math.random() * 6 + 1);
+    this.die2 = Math.floor(Math.random() * 6 + 1);
+  }
+
+  onToggleNight(event): void {
+    event.stopPropagation();
+    this.night = !this.night;
+  }
 }
