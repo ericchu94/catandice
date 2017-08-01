72d5465c93dd1e538207735975837d0b5eadb7c3
Eric Chu <eric@ericchu.net>
2017-08-01 09:56:32 -0400

    Use angular animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('night', [
+      state('true', style({
+        backgroundColor: 'black',
+        color: 'white',
+      })),
+      state('false', style({
+        backgroundColor: 'white',
+        color: 'black',
+      })),
+      transition('* => *', animate('1s')),
+    ]),
+    trigger('roll', [
+      state('true', style({
+        opacity: 0,
+      })),
+      state('false', style({
+        opacity: 1,
+      })),
+      transition('* => *', animate('0.5s')),
+    ])
+  ]
 })
+
 export class AppComponent {
   night: boolean = false;
   die1: number = 1;
@@ -12,20 +42,26 @@ export class AppComponent {
   firstDieRed: boolean = true;
   history: number[] = [];
   hidden: boolean = false;
+  rolling: boolean = false;
 
   ngOnInit(): void {
-    this.onRandom();
-    this.history = [];
+    this.random();
   }
 
-  async onRandom(): Promise<void> {
-    this.history.push(this.die1 + this.die2);
-    this.hidden = true;
-    await (new Promise(resolve => setTimeout(resolve, 250)));
+  onRoll(): void {
+    this.rolling = true;
+  }
+
+  onRollDone(): void {
+    if (this.rolling) {
+      this.random();
+      this.rolling = false;
+    }
+  }
+
+  random(): void {
     this.die1 = Math.floor(Math.random() * 6 + 1);
     this.die2 = Math.floor(Math.random() * 6 + 1);
-    this.hidden = false;
-    this.firstDieRed = !this.firstDieRed;
   }
 
   onToggleNight(event): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
